cf31f1ada6a8cd3eb9b144480bb3db921e93a2ef
Abby Beeler <abby.r.beeler@gmail.com>
2020-05-23 20:37:35 -0400

    trained model, removed flask

--- Inference.py
+++ Inference.py
@@ -1,19 +1,10 @@
-from flask import Flask,request,render_template
-import requests 
-import json
-from collections import OrderedDict
-import os
-import numpy as np
-import tensorflow as tf
-
-app =Flask(__name__)
-
 import sys
+
+import numpy as np
 
 if not 'texar_repo' in sys.path:
   sys.path += ['texar_repo']
 
-from config import *
 from model import *
 from preprocess import *
 
@@ -38,59 +29,64 @@ else:
     inferred_ids = predictions['sample_id'][:, :, 0]
 
 
-
-
 tokenizer = tokenization.FullTokenizer(
       vocab_file=os.path.join(bert_pretrain_dir, 'vocab.txt'),
       do_lower_case=True)
 
 
-sess = tf.Session()
-def infer_single_example(story,actual_summary,tokenizer):
-      example = {"src_txt":story,
-      "tgt_txt":actual_summary
-      }
-      features = convert_single_example(1,example,max_seq_length_src,max_seq_length_tgt,
-         tokenizer)
-      feed_dict = {
-      src_input_ids:np.array(features.src_input_ids).reshape(-1,1),
-      src_segment_ids : np.array(features.src_segment_ids).reshape(-1,1)
-
-      }
-
-      references, hypotheses = [], []
-      fetches = {
-      'inferred_ids': inferred_ids,
-      }
-      fetches_ = sess.run(fetches, feed_dict=feed_dict)
-      labels = np.array(features.tgt_labels).reshape(-1,1)
-      hypotheses.extend(h.tolist() for h in fetches_['inferred_ids'])
-      references.extend(r.tolist() for r in labels)
-      hypotheses = utils.list_strip_eos(hypotheses, eos_token_id)
-      references = utils.list_strip_eos(references[0], eos_token_id)
-      hwords = tokenizer.convert_ids_to_tokens(hypotheses[0])
-      rwords = tokenizer.convert_ids_to_tokens(references[0])
-
-      hwords = tx.utils.str_join(hwords).replace(" ##","")
-      rwords = tx.utils.str_join(rwords).replace(" ##","")
-      print("Original",rwords)
-      print("Generated",hwords)
-      return hwords
-
-@app.route("/results",methods=["GET","POST"])
-def results():
-	story = request.form['story']
-	summary = request.form['summary']
-	hwords = infer_single_example(story,summary,tokenizer)
-	return hwords
-
-
 if __name__=="__main__":
+    story = 'STORY'  # TODO
+
+
+    sess = tf.Session()
     sess.run(tf.global_variables_initializer())
     sess.run(tf.local_variables_initializer())
     sess.run(tf.tables_initializer())
     saver.restore(sess, tf.train.latest_checkpoint(model_dir))
-    app.run(host="0.0.0.0",port=1118,debug=False)
-    
 
+    tokens_a = tokenizer.tokenize(story)
+    # Modifies `tokens_a` and `tokens_b` in place so that the total
+    # length is less than the specified length.
+    # Account for [CLS], [SEP], [SEP] with "- 3"
+    if len(tokens_a) > max_seq_length_src - 2:
+        tokens_a = tokens_a[0:(max_seq_length_src - 2)]
 
+    tokens_src = []
+    segment_ids_src = []
+    tokens_src.append("[CLS]")
+    segment_ids_src.append(0)
+    for token in tokens_a:
+        tokens_src.append(token)
+        segment_ids_src.append(0)
+    tokens_src.append("[SEP]")
+    segment_ids_src.append(0)
+
+    input_ids_src = tokenizer.convert_tokens_to_ids(tokens_src)
+    input_mask_src = [1] * len(input_ids_src)
+
+    while len(input_ids_src) < max_seq_length_src:
+        input_ids_src.append(0)
+        input_mask_src.append(0)
+        segment_ids_src.append(0)
+
+    features = InputFeatures(src_input_ids=input_ids_src,src_input_mask=input_mask_src,src_segment_ids=segment_ids_src,
+                             tgt_input_ids=None,tgt_input_mask=None,tgt_labels=None)
+
+    feed_dict = {
+        src_input_ids:np.array(features.src_input_ids).reshape(-1,1),
+        src_segment_ids : np.array(features.src_segment_ids).reshape(-1,1)
+    }
+
+    hypotheses = []
+    fetches = {
+        'inferred_ids': inferred_ids,
+    }
+    fetches_ = sess.run(fetches, feed_dict=feed_dict)
+    labels = np.array(features.tgt_labels).reshape(-1,1)
+    hypotheses.extend(h.tolist() for h in fetches_['inferred_ids'])
+    hypotheses = utils.list_strip_eos(hypotheses, eos_token_id)
+    hwords = tokenizer.convert_ids_to_tokens(hypotheses[0])
+    hwords = tx.utils.str_join(hwords).replace(" ##","")
+
+    print("Original", story)
+    print("Generated",hwords)
